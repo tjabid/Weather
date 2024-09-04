8379f3d477863cbb56164d00b6f2a0778f0916b7
tjabid <TajjalleeAbid@gmail.com>
2024-09-04 09:47:00 +0400

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Abdul rahim on 04/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
